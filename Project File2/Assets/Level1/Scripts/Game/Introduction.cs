30524231b4d2e965aca683914245e3bfdd7b0728
jangfogs <hsa.61@hotmail.com>
2019-12-02 21:53:21 +0100

    no message

---
+++ Project File2/Assets/Level1/Scripts/Game/Introduction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Introduction : MonoBehaviour
+{
+
+    public void Page2()
+    {
+        if (true)
+        {
+            SceneManager.LoadScene("Introduction2");
+        }
+    }
+    public void Page1()
+    {
+        if (true)
+        {
+            SceneManager.LoadScene("Credits");
+        }
+    }
+    public void BackToMenu()
+    {
+        if (true)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
